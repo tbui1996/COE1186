20d2db86ab35d801331dc1d41a079a79c8d535a8
Thomas Bui <thomas1296@live.com>
2019-10-13 17:39:07 -0400

    YEET

---
+++ src/UserSelection/Controller.java
@@ -0,0 +1,26 @@
+package UserSelection;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML private ChoiceBox rate;
+//    @FXML private Label rateLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        rate.getItems().add("1x");
+        rate.getItems().add("10x");
+        rate.getItems().add("50x");
+        rate.setValue("1x");
+    }
+
+}
