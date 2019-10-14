ce16c1bc00a12a760a7477aa8c6474fa6ebd4eb5
Philip Seitz <pseitz08@gmail.com>
2019-10-13 20:32:12 -0400

    Fixing IntelliJ

--- src/tcss/ctc/Dispatch.java
+++ src/tcss/ctc/Dispatch.java
@@ -1,4 +1,28 @@
 package tcss.ctc;
 
 public class Dispatch {
+    private double SS;
+    private int auth;
+
+    public Dispatch() {
+        this.SS = 0;
+        this.auth = 0;
+    }
+
+    public Dispatch(float SS, int auth) {
+        this.SS = SS;
+        this.auth = auth;
+    }
+
+    public void setSS(float SS) {
+        this.SS = SS;
+    }
+
+    public void setAuth(int auth) {
+        this.auth = auth;
+    }
+
+    public void sendInfo() {
+
+    }
 }
---
+++ src/tcss/ctc/Train.java
@@ -0,0 +1,12 @@
+package tcss.ctc;
+
+public class Train {
+
+    private String name;
+    private double SS;
+    private int auth;
+
+    public Train(String name) {
+        this.name = name;
+    }
+}
