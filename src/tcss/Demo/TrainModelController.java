59eedf6d8d01e5db945dbdc57731cb8270235260
Wesley <wam34@pitt.edu>
2019-10-14 19:43:51 -0400

    Train Model for demo
    Signed-off-by: Wesley <wam34@pitt.edu>

--- src/tcss/Demo/TrainModelController.java
+++ src/tcss/Demo/TrainModelController.java
@@ -13,11 +13,13 @@ public class TrainModelController implements Initializable {
     @FXML private Label idLabel;
     @FXML private Label sSpeedLabel;
     @FXML private Label authLabel;
+    @FXML private Label speedLimitLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         idLabel.setText("ID: ");
         sSpeedLabel.setText("Suggested Speed: 0 mph");
         authLabel.setText("Authority: 0 Blocks");
+        speedLimitLabel.setText("Speed Limit: 0 mph");
     }
 }
