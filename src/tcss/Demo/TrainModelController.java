8c8616fbbfae6b11056bf51125a2d7105689d8a7
Wesley <wam34@pitt.edu>
2019-10-14 14:13:38 -0400

    Train Model UI - Demo
    Signed-off-by: Wesley <wam34@pitt.edu>

---
+++ src/tcss/Demo/TrainModelController.java
@@ -0,0 +1,23 @@
+package tcss.Demo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TrainModelController implements Initializable {
+
+    // UI variables
+    @FXML private Label idLabel;
+    @FXML private Label sSpeedLabel;
+    @FXML private Label authLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        idLabel.setText("ID: ");
+        sSpeedLabel.setText("Suggested Speed: 0 mph");
+        authLabel.setText("Authority: 0 Blocks");
+    }
+}
