55a7d3be2cb3f07a6d6f09e77d8bedb1e2eb30e7
Thomas Bui <thomas1296@live.com>
2019-10-16 17:22:15 -0400

    TC update

---
+++ src/tcss/TrackController/WaysideController.java
@@ -0,0 +1,12 @@
+package tcss.TrackController;
+
+public class WaysideController {
+    int line;
+    int blockId;
+    boolean iSswitch;
+    boolean iScrossing;
+    String status;
+    boolean lights;
+    boolean occupancy;
+    boolean heat;
+}
