980adbbe5cb73c67de14e49d1b642c75810a4e7d
Philip Seitz <pseitz08@gmail.com>
2019-11-13 16:32:03 -0500

    Set up branch

--- src/tcss/ctc/CTC.java
+++ src/tcss/ctc/CTC.java
@@ -5,21 +5,53 @@ import java.util.*;
 import tcss.trackmodel.TrackModel;
 import tcss.trainmodel.*;
 import tcss.trackcontroller.*;
+import tcss.trackmodel.*;
 
 public class CTC {
     TrackController TC1;
     ArrayList<Dispatch> dispatchList = new ArrayList<Dispatch>(); /*Number of Trains*/
     public ArrayList<TrainModel> trainList = new ArrayList<TrainModel>(); /*Number of Trains*/
 
+    //Temporary Red and Green Line setup for creating a Dispatch
+    private LinkedList<Block> redLine;
+    private LinkedList<Block> greenLine;
+    private String stationNames [];
+
+
     public CTC(TrackController track) {
 
         this.TC1 = track;
+
+        //Temporary Red and Green Line setup for creating a Dispatch
+        redLine = new LinkedList<Block>();
+        greenLine = new LinkedList<Block>();
+        stationNames = new String[5];
+        stationNames[0] = "Dormont";
+        stationNames[1] = "Station Square";
+        stationNames[2] = "First Ave";
+        stationNames[3] = "South Hills Junction";
+        stationNames[4] = "Swissvale";
+        for (int i=0; i < 5; i++) {
+            if (i%2 == 0) {
+                redLine.add(new Block());
+                redLine.get(i).setStation(new Station(stationNames[i]));
+            }
+            else {
+                greenLine.add(new Block());
+                greenLine.get(i).setStation(new Station(stationNames[i]));
+            }
+        }
 
     }
 
     public void createDispatch(String name, float SS, int auth, TrainModel train) {
         //TrainModel temp = new TrainModel(SS, auth, trainList.size(), 55);
         //this.trainList.add(new TrainModel(name, trainList.size()));
+        System.out.println("Red Line:");
+        //Start here and Fix this
+        for (int i = 0; i < 10; i++) {
+
+        }
         this.trainList.add(train);
         this.dispatchList.add(new Dispatch(SS, auth, this.trainList.get(this.trainList.size()-1)));
         this.dispatchList.get(this.dispatchList.size()-1).createSchedule();
