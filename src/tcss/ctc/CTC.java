38777a9e772aee55fb970f8aa0786bd4d591082a
Philip Seitz <pseitz08@gmail.com>
2019-11-17 19:27:03 -0500

    Set up branch

--- src/tcss/ctc/CTC.java
+++ src/tcss/ctc/CTC.java
@@ -5,25 +5,81 @@ import java.util.*;
 import tcss.trackmodel.TrackModel;
 import tcss.trainmodel.*;
 import tcss.trackcontroller.*;
+import tcss.trackmodel.*;
 
 public class CTC {
     TrackController TC1;
-    ArrayList<Dispatch> dispatchList = new ArrayList<Dispatch>(); /*Number of Trains*/
+    private ArrayList<Dispatch> dispatchList = new ArrayList<Dispatch>(); /*Number of Trains*/
     public ArrayList<TrainModel> trainList = new ArrayList<TrainModel>(); /*Number of Trains*/
+
+    //Temporary Red and Green Line setup for creating a Dispatch
+    private LinkedList<Block> redLine;
+    private LinkedList<Block> greenLine;
+    private String stationNames [];
+
+
+
 
     public CTC(TrackController track) {
 
         this.TC1 = track;
+
+        //Temporary Red and Green Line setup for creating a Dispatch
+        redLine = new LinkedList<Block>();
+        greenLine = new LinkedList<Block>();
+        stationNames = new String[5];
+        stationNames[0] = "Dormont";
+        stationNames[1] = "Shadyside";
+        stationNames[2] = "First Ave";
+        stationNames[3] = "South Hills Junction";
+        stationNames[4] = "Swissvale";
+        for (int i=0; i < 5; i++) {
+            Block temp = new Block();
+            temp.setStation(new Station(stationNames[i]));
+            if (i%2 == 0) {
+                redLine.add(temp);
+                greenLine.add(new Block());
+            }
+            else {
+                greenLine.add(temp);
+                redLine.add(new Block());
+            }
+        }
 
     }
 
     public void createDispatch(String name, float SS, int auth, TrainModel train) {
         //TrainModel temp = new TrainModel(SS, auth, trainList.size(), 55);
         //this.trainList.add(new TrainModel(name, trainList.size()));
+        //Start here and Fix this
+        for (int i = 0; i < redLine.size(); i++) {
+            System.out.println("Red Block " + i + ": " + redLine.get(i).toString());
+            System.out.println("Green Block " + i + ": " + greenLine.get(i).toString());
+        }
         this.trainList.add(train);
         this.dispatchList.add(new Dispatch(SS, auth, this.trainList.get(this.trainList.size()-1)));
+        this.dispatchList.get(this.dispatchList.size()-1).createSchedule();
+        this.dispatchList.get(this.dispatchList.size()-1).setRequests();
+        System.out.println(this.dispatchList.get(this.dispatchList.size()-1));
         sendNextStop(SS, auth, train.getID());
     }
+
+    //This checks dispatch list to see if a new suggested speed and authority need to be sent
+    public void checkDispatchList () {
+        for (int i = 0; i < dispatchList.size(); i++) {
+            Dispatch temp = dispatchList.get(i);
+            //if train is not dispatched yet
+            if (temp.getCurrStop() == -1) {
+                if (100 == temp.getDepartureTime()) { //This should be if departure time == current global time
+                    //temp.set
+                }
+            }
+            else {
+
+            }
+        }
+    }
+
     public TrackController getTrackController(int index){
         return this.TC1;
     }
--- src/tcss/ctc/Dispatch.java
+++ src/tcss/ctc/Dispatch.java
@@ -2,10 +2,19 @@ package tcss.ctc;
 
 import tcss.trainmodel.TrainModel;
 
+
 public class Dispatch {
     private float SS;
     private int auth;
+    private Schedule schedule;
+    public String trainName;
+    private int mode;
     private TrainModel train;
+    private int currStop = -1;
+    private float speedList [];
+    private int authList [];
+    private int arrivalTime;
+    private int departureTime;
 
     public Dispatch() {
         this.SS = 0;
@@ -16,6 +25,40 @@ public class Dispatch {
         this.SS = SS;
         this.auth = auth;
         this.train = train;
+    }
+
+    public void createSchedule() {
+        this.schedule = new Schedule(1);
+        this.schedule.addStop("Shadyside", (float) 3.7);
+        //this.schedule.addStop("Herron Ave", (float) 2.3);
+        this.schedule.addStop("Swissvale", (float) 1.5);
+        //this.schedule.addStop("Penn Station", (float) 1.8);
+        //this.schedule.addStop("Steel Plaza", (float) 2.1);
+        //this.schedule.addStop("First Ave", (float) 2.1);
+        //this.schedule.addStop("Station Square", (float) 1.7);
+        //this.schedule.addStop("South Hills Junction", (float) 2.3);
+        this.arrivalTime = 500;
+        this.departureTime = 100;
+    }
+
+    public void setRequests() {
+        speedList = new float[schedule.getStopNums()];
+        authList = new int[schedule.getStopNums()];
+
+
+        for (int i = 0; i < schedule.getStopNums(); i++) {
+            //(distance between blocks) / ((station w/ dwell) - dwell), unit is blocks/sec
+            if (i == 0) {
+                speedList[i] = (1 / (float) (3.7 * 60 - 35)); //stationToYard((schedule.getStopName(i)) / schedule.getStopDwell(i)*60 - 35
+                authList[i] = 1; //stationToYard(schedule.getStopName(i))
+            }
+            else {
+                //speedList[i] = stationToStation((schedule.getStopName(i-1),schedule.getStopName(i)) / schedule.getStopDwell(i)*60 - 35;
+                //authList = stationToStation(schedule.getStopName(i-1), schedule.getStopName(i));
+                speedList[i] = (3 / (float) (1.5 * 60 - 35));
+                authList[i] = 3;
+            }
+        }
     }
 
     public void setSS(float SS) {
@@ -32,12 +75,31 @@ public class Dispatch {
     public float getSS(){
         return this.SS;
     }
+
     public int getAuth() {
         return this.auth;
     }
 
+    public int getCurrStop() {
+        return this.currStop;
+    }
+
+    public void setCurrStop(int i) {
+        this.currStop = i;
+        /**/
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getDepartureTime() {
+        return departureTime;
+    }
+
 
     public String toString() {
-        return "ID: " + this.train.getID() + "\nSuggested Speed: " + this.train.getSSpeed() + "\nAuthority: " + this.train.getAuthority();
+        return "ID: " + this.train.getID() + "\nSuggested Speed: " + this.train.getSSpeed() + "\nAuthority: " + this.train.getAuthority() +
+                "Schedule: " + this.schedule;
     }
 }
