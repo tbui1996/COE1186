d93e5c569b2832de95d730332c5a5d3d5c067322
Wesley <wam34@pitt.edu>
2019-11-21 11:20:38 -0500

    work

---
+++ tests/tcss/trackmodel/TrackModelTest.java
@@ -0,0 +1,43 @@
+package tcss.trackmodel;
+
+class TrackModelTest {
+
+    static TrackModel tm;
+
+    @BeforeAll
+    static void setUp() throws Exception{
+        tm = new TrackModel();
+    }
+
+    @Test
+    void setEBrake() {
+    }
+
+    @Test
+    void setSBrake() {
+    }
+
+    @Test
+    void calculateVelocity1() {
+    }
+
+    @Test
+    void calcForces1() {
+    }
+
+    @Test
+    void calcAccel1() {
+    }
+
+    @Test
+    void setLights() {
+    }
+
+    @Test
+    void setBeacon() {
+    }
+
+    @Test
+    void setDoor() {
+    }
+}
